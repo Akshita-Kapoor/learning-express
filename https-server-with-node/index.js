59826098a98f2a18cb07ac9322404888cbf14450
Akshita <ak.akshita21@gmail.com>
2024-11-15 12:34:18 -0700

    appending a file

--- https-server-with-node/index.js
+++ https-server-with-node/index.js
@@ -1,13 +1,22 @@
 const http = require('http');
+const fs = require('fs');
 
 /* 
     createServer() function takes a callback function which process any incoming requests.
     callback function takes 2 arguments - req and res.
-
 */
 const myServer = http.createServer((req, res) => {
     // req object has information that what the request is, what type of request is, (metadata) etc.
-    res.end('Hello from server');
+    const log = `${Date.now()}: ${req.url} New request received\n`;
+    fs.appendFile("log.txt", log, (err, data) => {
+        switch(req.url) {
+            case '/': res.end('Home page');
+            break;
+            case '/about': res.end('About page');
+            break;
+            default: res.end('Not found');
+        }
+    });
 });
 
 // port is like a door where this server will run
