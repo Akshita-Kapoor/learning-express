83965567de3b973b5dfd884d7d6c24a2577afb39
Akshita <ak.akshita21@gmail.com>
2024-11-19 12:01:27 -0700

    post api with middleware

--- Project-1/index.js
+++ Project-1/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const users = require('./MOCK_DATA.json');
+const bodyParser = require('body-parser');
+
+const fs = require('fs');
 
 const app = express();
 const PORT = 8000;
+app.use(bodyParser.json());
 
 //GET request
 app.get('/api/users', (req, res) => {
@@ -18,10 +22,25 @@ app.get('/users', (req, res) => {
     return res.send(html);
 });
 
-app.get('/api/users/:id', (req, res) => {
-    const paramId = req.params.id; 
-    const user = users.find((item) => item.id == paramId); 
-    return res.json(user);
+app.route('/api/users/:id')
+    .get((req, res) => {
+        const paramId = req.params.id; 
+        const user = users.find((item) => item.id == paramId); 
+        return res.json(user);
+    })
+    .patch((req, res) => { 
+        return res.json({status: "pending"});
+    })
+    .delete((req, res) => {
+        return res.json({status: "pending"});
+    })
+
+app.post('/api/users', (req, res) => {
+    const body = req.body;    
+    users.push({...body, id: users.length + 1});
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+        return res.json({status: "success", id: users.length});
+    })
 })
 
 app.listen(PORT, () => console.log(`server listening on port ${PORT}`));
